c485779418a59e3dbc9362e67f4caf2dd321514d
Alex Novak <alexwnovak@gmail.com>
2019-03-08 21:51:22 -0500

    Default behavior is writing no output

--- tests/Write-StatusBar.Tests.ps1
+++ tests/Write-StatusBar.Tests.ps1
@@ -1,7 +1,17 @@
 . $PSScriptRoot\Shared.ps1
 
-Describe "Write-StatusBar" {
+Describe 'Write-StatusBar' {
     BeforeEach {
         Reset-GlobalState
+    }
+
+    Mock -ModuleName Terminoid Write-Host { }
+
+    Context 'There are no bars' {
+        It 'writes an empty string' {
+            Write-StatusBar
+
+            Assert-MockCalled -ModuleName Terminoid Write-Host -Times 0
+        }
     }
 }
