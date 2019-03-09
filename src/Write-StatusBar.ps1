03ea44e00e0fbd925186093624f80a28d0483ffd
Alex Novak <alexwnovak@gmail.com>
2019-03-09 09:43:55 -0500

    Write-StatusBar can now write a single bar

--- src/Write-StatusBar.ps1
+++ src/Write-StatusBar.ps1
@@ -1,3 +1,12 @@
 function Write-StatusBar {
-    
+    if ( $BarSegments.Count -eq 0 ) {
+        return
+    }
+
+    Write-Color " $(& $BarSegments[0].Function) " `
+        -Foreground $BarSegments[0].ForegroundColor `
+        -Background $BarSegments[0].BackgroundColor `
+        -NoNewline
+
+    Write-Color "$([char]0xE0B0)" -Foreground $BarSegments[0].BackgroundColor
 }
--- tests/Write-StatusBar.Tests.ps1
+++ tests/Write-StatusBar.Tests.ps1
@@ -5,13 +5,28 @@ Describe 'Write-StatusBar' {
         Reset-GlobalState
     }
 
-    Mock -ModuleName Terminoid Write-Host { }
+    Mock -ModuleName Terminoid Write-Color { }
 
-    Context 'There are no bars' {
-        It 'writes an empty string' {
-            Write-StatusBar
+    It 'writes nothing if there are no bars' {
+        Write-StatusBar
 
-            Assert-MockCalled -ModuleName Terminoid Write-Host -Times 0
+        Assert-MockCalled -ModuleName Terminoid Write-Color -Times 0
+    }
+
+    It 'has one bar and writes the value with the joiner character' {
+        Push-BarSegment -ForegroundColor 255,255,255 -BackgroundColor 0,0,0 -Text 'segment-text'
+        Write-StatusBar
+
+        Assert-MockCalled -ModuleName Terminoid Write-Color -ParameterFilter {
+            $Object -eq ' segment-text ' -and `
+            $null -eq ($Foreground | Compare-Object @(255,255,255)) -and `
+            $null -eq ($Background | Compare-Object @(0,0,0)) -and `
+            $NoNewline -eq $true
+        }
+
+        Assert-MockCalled -ModuleName Terminoid Write-Color -ParameterFilter {
+            $Object -eq "$([char]0xE0B0)" -and `
+            $null -eq ($Foreground | Compare-Object @(0,0,0))
         }
     }
 }
