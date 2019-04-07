2a35d023d23752e673a8b55e1da9d734989c1d39
Alex Novak <alexwnovak@gmail.com>
2019-04-07 18:34:12 -0400

    Set-Location tests no longer need InModuleScope
    Instead of mocking the call to the original Set-Location function, now it creates real directories in the TestDrive provider. This means we're really changing the working directory, which we set back after each test.

--- src/Set-Location.ps1
+++ src/Set-Location.ps1
@@ -1,24 +1,11 @@
-function Set-LocationInternal {
+function Set-Location {
     param (
         $Path
     )
 
     Microsoft.PowerShell.Management\Set-Location $Path
-}
 
-function ConvertTo-FullPath( $Path ) {
-    [System.IO.Path]::GetFullPath( $Path )
-}
-
-function Set-Location {
-    [CmdletBinding()]
-    param (
-        $Path
-    )
-
-    Set-LocationInternal $Path
-
-    $newLocation = ConvertTo-FullPath $Path
+    $newLocation = Convert-Path $Path
 
     [void] (New-Event -SourceIdentifier Terminoid.LocationChanged -MessageData $newLocation)
     [void] $LocationHistory.Add( $newLocation )
--- tests/Set-Location.Tests.ps1
+++ tests/Set-Location.Tests.ps1
@@ -1,46 +1,42 @@
 . $PSScriptRoot\Shared.ps1
 
-InModuleScope 'Terminoid' {
-    Describe 'Set-Location' {
-        Mock ConvertTo-FullPath { }
+Describe 'Set-Location' {
+    BeforeEach {
+        $oldPwd = $PWD
 
-        BeforeEach {
-            Clear-LocationHistory
-            Get-EventSubscriber -SourceIdentifier Terminoid.LocationChanged -ErrorAction SilentlyContinue | Unregister-Event
+        Clear-LocationHistory
+        Get-EventSubscriber -SourceIdentifier Terminoid.LocationChanged -ErrorAction SilentlyContinue | Unregister-Event
+    }
+
+    AfterEach {
+        Microsoft.PowerShell.Management\Set-Location $oldPwd
+    }
+
+    It 'navigates to the new location' {
+        New-Item TestDrive:\SetLocation_SettingTheLocation -ItemType Directory
+
+        Set-Location -Path TestDrive:\SetLocation_SettingTheLocation
+
+        $PWD | Should -Be 'TestDrive:\SetLocation_SettingTheLocation'
+    }
+
+    It 'raises an event with the new location' {
+        New-Item TestDrive:\SetLocation_AnEventShouldBeRaised -ItemType Directory
+
+        Register-EngineEvent Terminoid.LocationChanged -Action {
+            Set-Content TestDrive:\NewLocation.txt $Event.MessageData
         }
 
-        Context 'Setting the location' {
-            Mock Set-LocationInternal { }
+        Set-Location -Path TestDrive:\SetLocation_AnEventShouldBeRaised
 
-            It 'sets the new location' {
-                Terminoid\Set-Location -Path 'new-location'
-                Assert-MockCalled Set-LocationInternal -ParameterFilter { $Path -eq 'new-location' }
-            }
-        }
+        Get-Content TestDrive:\NewLocation.txt | Should -BeLike '*\SetLocation_AnEventShouldBeRaised'
+    }
 
-        Context 'An event should be raised after navigation' {
-            Mock Set-LocationInternal { }
-            Mock ConvertTo-FullPath { 'C:\new-location' }
+    It 'records the destination to location history' {
+        New-Item TestDrive:\SetLocation_TheLocationShouldBeRecorded -ItemType Directory
 
-            It 'raises an event with the new fully-qualified path' {
-                Register-EngineEvent Terminoid.LocationChanged -Action {
-                    Set-Content TestDrive:\NewLocation.txt $Event.MessageData
-                }
+        Set-Location -Path 'TestDrive:\SetLocation_TheLocationShouldBeRecorded'
 
-                Terminoid\Set-Location -Path 'new-location'
-
-                Get-Content TestDrive:\NewLocation.txt | Should -Be 'C:\new-location'
-            }
-        }
-
-        Context 'The location should be recorded' {
-            Mock Set-LocationInternal { $LocationHistory.Count | Should -Be 0 }
-            Mock ConvertTo-FullPath { 'C:\new-location' }
-
-            It 'records the new location' {
-                Terminoid\Set-Location -Path 'new-location'
-                Get-LocationHistory | Should -Contain 'C:\new-location'
-            }
-        }
+        Get-LocationHistory | Should -BeLike '*\SetLocation_TheLocationShouldBeRecorded'
     }
 }
