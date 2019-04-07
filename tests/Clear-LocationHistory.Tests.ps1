8f784be79837f5db919549350d28bc840d57c7b5
Alex Novak <alexwnovak@gmail.com>
2019-04-07 10:54:43 -0400

    Fleshing out Clear-Location

--- tests/Clear-LocationHistory.Tests.ps1
+++ tests/Clear-LocationHistory.Tests.ps1
@@ -1,5 +1,18 @@
 . $PSScriptRoot\Shared.ps1
 
-Describe 'Clear-LocationHistory' {
+InModuleScope 'Terminoid' {
+    Mock Set-LocationInternal { }
+    Mock ConvertTo-FullPath { param ( $Path ) $Path }
 
+    Describe 'Clear-LocationHistory' {
+        It 'removes all history items' {
+            Set-Location 'new-location-one'
+            Set-Location 'new-location-two'
+            Set-Location 'new-location-three'
+
+            Clear-History
+
+            Get-LocationHistory | Should -BeNullOrEmpty
+        }
+    }
 }
