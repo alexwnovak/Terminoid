ba5f311e0fa967bd5f516bf55e2409ddf3f7fde8
Alex Novak <alexwnovak@gmail.com>
2019-04-06 22:02:27 -0400

    Push-Location now raises the LocationChanged event

--- src/Push-Location.ps1
+++ src/Push-Location.ps1
@@ -6,10 +6,18 @@ function Push-LocationInternal {
     Microsoft.PowerShell.Management\Push-Location $Path
 }
 
+function ConvertTo-FullPath( $Path ) {
+    [System.IO.Path]::GetFullPath( $Path )
+}
+
 function Push-Location {
     param (
         $Path
     )
 
     Push-LocationInternal $Path
+
+    $newLocation = ConvertTo-FullPath $Path
+
+    [void] (New-Event -SourceIdentifier Terminoid.LocationChanged -MessageData $newLocation)
 }
--- tests/Push-Location.Tests.ps1
+++ tests/Push-Location.Tests.ps1
@@ -2,12 +2,33 @@
 
 InModuleScope 'Terminoid' {
     Describe 'Push-Location' {
+        Mock ConvertTo-FullPath { }
+
+        BeforeEach {
+            Get-EventSubscriber -SourceIdentifier Terminoid.LocationChanged -ErrorAction SilentlyContinue | Unregister-Event
+        }
+
         Context 'Setting the location' {
             Mock Push-LocationInternal { }
 
             It 'sets the new location' {
                 Terminoid\Push-Location -Path 'new-location'
                 Assert-MockCalled Push-LocationInternal -ParameterFilter { $Path -eq 'new-location' }
+            }
+        }
+
+        Context 'An event should be raised after navigation' {
+            Mock Push-LocationInternal { }
+            Mock ConvertTo-FullPath { 'C:\new-location' }
+
+            It 'raises an event with the new fully-qualified path' {
+                Register-EngineEvent Terminoid.LocationChanged -Action {
+                    Set-Content TestDrive:\PushLocation-NewLocation.txt $Event.MessageData
+                }
+
+                Terminoid\Push-Location -Path 'new-location'
+
+                Get-Content TestDrive:\PushLocation-NewLocation.txt | Should -Be 'C:\new-location'
             }
         }
     }
