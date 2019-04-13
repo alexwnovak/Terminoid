5d699ccd82e4f1244a769b7d405c78566e3106d7
Alex Novak <alexwnovak@gmail.com>
2019-04-13 16:47:56 -0400

    Splitting the module into functions
    Also ditching the "stat" alias.
    
    Leaving the dot-source outside a function since it seems like it's scoped to the function it's in. When I tried dot-sourcing in a function, the functions weren't found later after it ended.

--- src/Terminoid.psm1
+++ src/Terminoid.psm1
@@ -1,22 +1,25 @@
 Get-ChildItem $PSScriptRoot\*.ps1 -Recurse | ForEach-Object { . $_ }
 
-[System.Collections.ArrayList] $script:BarSegments = @()
-[System.Collections.ArrayList] $script:LocationHistory = @()
-$script:FormatterTable = New-Object System.Collections.Stack
+function InitializeInternalVariables {
+    [System.Collections.ArrayList] $script:BarSegments = @()
+    [System.Collections.ArrayList] $script:LocationHistory = @()
+    $script:FormatterTable = New-Object System.Collections.Stack
 
-$script:DefaultDetailReader = $function:DefaultFileDetailReader
+    $script:DefaultDetailReader = $function:DefaultFileDetailReader
 
-$script:SpecialCharTable = @{
-    Joiner = [char]0xE0B0
+    $script:SpecialCharTable = @{
+        Joiner = [char]0xE0B0
+    }
+
+    [System.Collections.ArrayList] $script:DetailReaderTable = @()
+    Reset-DetailReader
 }
 
-[System.Collections.ArrayList] $script:DetailReaderTable = @()
-Reset-DetailReader
-
-New-Alias -Name stat -Value Get-FileDetail
-
-Get-ChildItem $PSScriptRoot\public\*.ps1 -Recurse | ForEach-Object {
-    Export-ModuleMember -Function $_.BaseName
+function ExportPublicFunctions {
+    Get-ChildItem $PSScriptRoot\public\*.ps1 -Recurse | ForEach-Object {
+        Export-ModuleMember -Function $_.BaseName
+    }
 }
 
-Export-ModuleMember -Alias stat
+InitializeInternalVariables
+ExportPublicFunctions
