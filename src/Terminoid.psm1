3f6609632180674ac935b320762696f2845da384
Alex Novak <alexwnovak@gmail.com>
2019-04-13 16:38:19 -0400

    Now automatically exporting all functions under /public
    This requires that a convention is followed--the file name is the name of the main function to export, since this is how it's determined what name to export.

--- src/Terminoid.psm1
+++ src/Terminoid.psm1
@@ -15,27 +15,8 @@ Reset-DetailReader
 
 New-Alias -Name stat -Value Get-FileDetail
 
-Export-ModuleMember -Function Clear-BarSegments
-Export-ModuleMember -Function Clear-FormatterTable
-Export-ModuleMember -Function Clear-LocationHistory
-Export-ModuleMember -Function Format-ChildItem
-Export-ModuleMember -Function Format-Date
-Export-ModuleMember -Function Format-Output
-Export-ModuleMember -Function Format-Size
-Export-ModuleMember -Function Get-BarSegments
-Export-ModuleMember -Function Get-FileDetail
-Export-ModuleMember -Function Get-LocationHistory
-Export-ModuleMember -Function Get-SpecialChar
-Export-ModuleMember -Function Hide-Cursor
-Export-ModuleMember -Function Pop-BarSegment
-Export-ModuleMember -Function Push-BarSegment
-Export-ModuleMember -Function Push-Location
-Export-ModuleMember -Function Register-ChildItemFormatter
-Export-ModuleMember -Function Reset-DefaultFileDetailReader
-Export-ModuleMember -Function Reset-DetailReader
-Export-ModuleMember -Function Register-FileDetailReader
-Export-ModuleMember -Function Set-Location
-Export-ModuleMember -Function Set-DefaultFileDetailReader
-Export-ModuleMember -Function Show-Cursor
-Export-ModuleMember -Function Write-StatusBar
+Get-ChildItem $PSScriptRoot\public\*.ps1 -Recurse | ForEach-Object {
+    Export-ModuleMember -Function $_.BaseName
+}
+
 Export-ModuleMember -Alias stat
