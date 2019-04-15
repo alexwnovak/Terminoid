3d0d3722b8e499ec224bf4891d08d2f25c711030
Alex Novak <alexwnovak@gmail.com>
2019-04-15 15:53:56 -0400

    Setting static analysis fail threshold to warning

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,6 +1,7 @@
 Properties {
     $PSBPreference.Test.CodeCoverage.Enabled = $true
     $PSBPreference.Test.CodeCoverage.Threshold = 1
+    $PSBPreference.Test.ScriptAnalysis.FailBuildOnSeverityLevel = 'Warning'
 }
 
 Task Test -FromModule PowerShellBuild -Version '0.3.0'
