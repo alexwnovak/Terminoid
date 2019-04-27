f180b6455507fee1d12a914305aa5dedf583663e
Alex Novak <alexwnovak@gmail.com>
2019-04-27 14:52:45 -0400

    Now generates a test to show that there are only N exposed functions
    This ensures that as the public API surface changes (especially if things are removed), a test is triggered. Removing commands is broken backward compatibilty, and this'll catch it. This also will error if new functions are added. In this case, the test should be regenerated, since addition is OK, and that would be the new stable normal.

--- build/GenerateCompatibilityTests.ps1
+++ build/GenerateCompatibilityTests.ps1
@@ -7,3 +7,51 @@ param (
     [string]
     $OutputPath
 )
+
+$script:IndentationLevel = 0
+$script:IndentSpaces = 4
+$script:ModuleName = [System.IO.Path]::GetFileNameWithoutExtension( $Psd1File )
+
+function Push-Indentation {
+    $script:IndentationLevel++
+}
+
+function Pop-Indentation {
+    if ( $script:IndentationLevel -ge 0 ) {
+        $script:IndentationLevel--
+    }
+}
+
+function Write-Indented( $Text ) {
+    $leading = ' ' * ($script:IndentationLevel * $script:IndentSpaces)
+    "$leading$Text"
+}
+
+function Write-FunctionNameTests {
+    Write-Indented "Describe 'Function names' {"
+
+    Push-Indentation
+    Write-Indented "Context 'The module exposes only the expected functions' {"
+
+    Push-Indentation
+
+    $commands = Get-Command -Module $ModuleName
+
+    Write-Indented "It 'exposes exactly $($commands.Count) commands' {"
+
+    Push-Indentation
+    Write-Indented "(Get-Command -Module $ModuleName).Count | Should -Be $($commands.Count)"
+    Pop-Indentation
+
+    Write-Indented "}"
+    Pop-Indentation
+
+    Write-Indented "}"
+    Pop-Indentation
+
+    Write-Indented "}"
+}
+
+Import-Module $Psd1File -Force
+
+Write-FunctionNameTests | Out-File $OutputPath\FunctionNames.Tests.ps1
