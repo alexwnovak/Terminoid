66d75e3889dbc302776e1acf7d7c801d3759caf4
Alex Novak <alexwnovak@gmail.com>
2019-04-28 16:05:22 -0400

    Build front-end can now be run from another directory
    Now uses $PSScriptRoot to correctly locate the right stuff, breaking the dependency on running it from the right working directory.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,6 @@
-powershell -NoProfile -Command {
-    & ./build/Install-Dependencies.ps1
-    Invoke-psake
+powershell -NoProfile -Args $PSScriptRoot -Command {
+    param ( $ScriptRoot )
+
+    & $ScriptRoot/build/Install-Dependencies.ps1
+    Invoke-psake -buildFile $ScriptRoot/psakefile.ps1
 }
