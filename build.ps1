58f866f24ea131d08bbe96581a4c8c16e6e197a7
Alex Novak <alexwnovak@gmail.com>
2019-04-30 22:54:19 -0400

    WIP

--- build.ps1
+++ build.ps1
@@ -4,5 +4,7 @@ powershell -NoProfile -Args $PSScriptRoot -Command {
     & $ScriptRoot/build/Install-Dependencies.ps1
     Invoke-psake -buildFile $ScriptRoot/psakefile.ps1
 
+    $exitCode = [int]( -not $psake.build_success )
+    Write-Host "Exiting with $exitCode and psake is $psake"
     exit ( [int]( -not $psake.build_success ) )
 }
