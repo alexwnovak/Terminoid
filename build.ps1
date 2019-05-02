bde7449550c63295b567f44f5d14a6b4120f2264
Alex Novak <alexwnovak@gmail.com>
2019-05-01 22:41:10 -0400

    Build script can now exit with an error code

--- build.ps1
+++ build.ps1
@@ -1,6 +1,16 @@
-powershell -NoProfile -Args $PSScriptRoot -Command {
-    param ( $ScriptRoot )
+param (
+    [switch]
+    $ExitOnFailure
+)
 
-    & $ScriptRoot/build/Install-Dependencies.ps1
-    Invoke-psake -buildFile $ScriptRoot/psakefile.ps1
+if ( $ExitOnFailure ) {
+    & $PSScriptRoot/build/Install-Dependencies.ps1
+    Invoke-psake -buildFile $PSScriptRoot/psakefile.ps1
+    exit ( [int]( -not $psake.build_success ) )
+} else {
+    powershell -NoProfile -Args $PSScriptRoot -Command {
+        param ( $ScriptRoot )
+        & $ScriptRoot/build/Install-Dependencies.ps1
+        Invoke-psake -buildFile $ScriptRoot/psakefile.ps1
+    }
 }
