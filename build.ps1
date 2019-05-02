91e45c58b8b4178db721eed2572263d7521d744f
Alex Novak <alexwnovak@gmail.com>
2019-05-01 22:23:38 -0400

    WIP

--- build.ps1
+++ build.ps1
@@ -1,14 +1,17 @@
-powershell -NoProfile -Args $PSScriptRoot -Command {
-    param ( $ScriptRoot )
+# powershell -NoProfile -Args $PSScriptRoot -Command {
+#     param ( $ScriptRoot )
+
+#     & $ScriptRoot/build/Install-Dependencies.ps1
+#     Invoke-psake -buildFile $ScriptRoot/psakefile.ps1
+
+#     $exitCode = [int]( -not $psake.build_success )
+#     Write-Host "Exiting with $exitCode and psake is $psake"
+
+#     # if ( -not $psake.build_success ) {
+#     #     throw 'Fail build'
+#     # }
+#     # exit ( [int]( -not $psake.build_success ) )
+# }
 
     & $ScriptRoot/build/Install-Dependencies.ps1
     Invoke-psake -buildFile $ScriptRoot/psakefile.ps1
-
-    $exitCode = [int]( -not $psake.build_success )
-    Write-Host "Exiting with $exitCode and psake is $psake"
-
-    if ( -not $psake.build_success ) {
-        throw 'Fail build'
-    }
-    # exit ( [int]( -not $psake.build_success ) )
-}
