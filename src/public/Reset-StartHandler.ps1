b9249d3bf1a11fb4ecf453501753fdbcbf322683
Alex Novak <alexwnovak@gmail.com>
2019-05-06 19:50:49 -0400

    Reset-StartHandler now supports -WhatIf
    Also regenerating compat tests.

--- src/public/Reset-StartHandler.ps1
+++ src/public/Reset-StartHandler.ps1
@@ -1,3 +1,8 @@
 function Reset-StartHandler {
-    [System.Collections.ArrayList] $script:StartHandlerTable = @()
+    [CmdletBinding( SupportsShouldProcess )]
+    param ()
+
+    if ( $PSCmdlet.ShouldProcess( 'Resetting custom start handlers' ) ) {
+        [System.Collections.ArrayList] $script:StartHandlerTable = @()
+    }
 }
