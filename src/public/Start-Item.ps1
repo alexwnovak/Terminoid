5ecd9b3f5ea2004b4e413e27fd05c909b0329b5e
Alex Novak <alexwnovak@gmail.com>
2019-05-06 22:40:13 -0400

    Start-Item now supports -WhatIf

--- src/public/Start-Item.ps1
+++ src/public/Start-Item.ps1
@@ -1,14 +1,19 @@
 function Start-Item {
+    [CmdletBinding( SupportsShouldProcess )]
     param (
         $Item
     )
 
     foreach ( $handler in $script:StartHandlerTable ) {
         if ( & $handler.Predicate $Item ) {
-            & $handler.Function $Item
-            return
+            if ( $PSCmdlet.ShouldProcess( 'Starting item with custom handler' ) ) {
+                & $handler.Function $Item
+                return
+            }
         }
     }
 
-    & $script:DefaultStartHandler $Item
+    if ( $PSCmdlet.ShouldProcess( 'Starting item with default handler' ) ) {
+        & $script:DefaultStartHandler $Item
+    }
 }
