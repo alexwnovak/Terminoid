79681bfa1cdaf15791cab613d81ddc1294f3fc62
Alex Novak <alexwnovak@gmail.com>
2019-05-18 18:27:30 -0400

    +RegionRenderer
    This class operates on the Region class's data and coordinates with the ConsoleContext to put it on the screen.

---
+++ src/Terminoid/RegionRenderer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Terminoid.Native;
+
+namespace Terminoid
+{
+   public static class RegionRenderer
+   {
+      public static Region Read( int x, int y, int width, int height )
+      {
+         var region = new Region( width, height );
+
+         for ( int row = 0; row < height; row++ )
+         {
+            var charBuffer = ConsoleContext.ReadChars( x, y + row, width );
+            region.SetCharLine( row, charBuffer );
+
+            var attrBuffer = ConsoleContext.ReadAttrs( x, y + row, width );
+            region.SetAttrLine( row, attrBuffer );
+         }
+
+         return region;
+      }
+
+      public static void Render( Region region, int x, int y )
+      {
+         var charBuffer = region.GetCharBuffer().AsSpan();
+         var attrBuffer = region.GetAttrBuffer().AsSpan();
+
+         for ( int row = 0; row < region.Height; row++ )
+         {
+            var charRow = charBuffer.Slice( row * region.Width, region.Width );
+            var attrRow = attrBuffer.Slice( row * region.Width, region.Width );
+
+            ConsoleContext.WriteChars( x, y + row, charRow, attrRow );
+         }
+      }
+   }
+}
