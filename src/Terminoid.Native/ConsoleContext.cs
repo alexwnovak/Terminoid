1386d1094afcfa6b891df0731ca1a7d1fda08e53
Alex Novak <alexwnovak@gmail.com>
2019-05-18 18:14:51 -0400

    +ConsoleContext

---
+++ src/Terminoid.Native/ConsoleContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Terminoid.Native
+{
+   public static class ConsoleContext
+   {
+      private static IntPtr _handle = NativeMethods.GetStdHandle( NativeMethods.STD_OUTPUT_HANDLE );
+
+      public unsafe static void WriteChars( int x, int y, ReadOnlySpan<char> slice, ReadOnlySpan<ushort> attr )
+      {
+         fixed ( char* ptr = slice )
+         {
+            NativeMethods.WriteConsoleOutputCharacter(
+               _handle,
+               ptr,
+               (uint) slice.Length,
+               new COORD( (short) x, (short) y ),
+               out _ );
+         }
+
+         fixed ( ushort* ptr = attr )
+         {
+            NativeMethods.WriteConsoleOutputAttribute(
+               _handle,
+               ptr,
+               (uint) attr.Length,
+               new COORD( (short) x, (short) y ),
+               out _ );
+         }
+      }
+
+      public static char[] ReadChars( int x, int y, int length )
+      {
+         var buffer = new char[length];
+
+         NativeMethods.ReadConsoleOutputCharacter(
+            _handle,
+            buffer,
+            (uint) length,
+            new COORD( (short) x, (short) y ),
+            out _ );
+
+         return buffer;
+      }
+
+      public static ushort[] ReadAttrs( int x, int y, int length )
+      {
+         var attrs = new ushort[length];
+
+         NativeMethods.ReadConsoleOutputAttribute(
+            _handle,
+            attrs,
+            (uint) length,
+            new COORD( (short) x, (short) y ),
+            out _ );
+
+         return attrs;
+      }
+   }
+}
