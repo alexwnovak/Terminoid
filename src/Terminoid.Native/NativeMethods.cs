eae1099a0d20aaa914fc8d38718f7cc1785b3cf1
Alex Novak <alexwnovak@gmail.com>
2019-05-18 18:13:26 -0400

    +NativeMethods for Win32 interop
    This isn't exposed outside the assembly, but instead is exposed through the ConsoleContext class (upcoming). It avoids unsafe code and isolates callers from interop.

---
+++ src/Terminoid.Native/NativeMethods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Terminoid.Native
+{
+   internal static class NativeMethods
+   {
+      public const int STD_OUTPUT_HANDLE = -11;
+
+      [DllImport( "kernel32.dll", SetLastError = true )]
+      public static extern IntPtr GetStdHandle( int nStdHandle );
+
+      [DllImport( "kernel32.dll" )]
+      public static extern bool ReadConsoleOutputCharacter(
+         IntPtr hConsoleOutput,
+         [Out] char[] lpCharacter,
+         uint nLength,
+         COORD dwReadCoord,
+         out uint lpNumberOfCharsRead );
+
+      [DllImport( "kernel32.dll", CharSet = CharSet.Unicode )]
+      public unsafe static extern bool WriteConsoleOutputCharacter(
+         IntPtr hConsoleOutput,
+         char* lpCharacter,
+         uint nLength,
+         COORD dwWriteCoord,
+         out uint lpNumberOfCharsWritten );
+
+      [DllImport( "kernel32.dll" )]
+      public static extern bool ReadConsoleOutputAttribute(
+         IntPtr hConsoleOutput,
+         [Out] ushort[] lpAttribute,
+         uint nLength,
+         COORD dwReadCoord,
+         out uint lpNumberOfCharsRead );
+
+      [DllImport( "kernel32.dll" )]
+      public unsafe static extern bool WriteConsoleOutputAttribute(
+         IntPtr hConsoleOutput,
+         ushort* lpAttribute,
+         uint nLength,
+         COORD dwWriteCoord,
+         out uint lpNumberOfAttrsWritten );
+   }
+}
