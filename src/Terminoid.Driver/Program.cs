0c96112034ebdd02f0f9e0e8e26f132bc3cabdfc
Alex Novak <alexwnovak@gmail.com>
2019-05-19 14:14:48 -0400

    +Simple Driver project to test out new things visually
    Handy for throwing together an idea to see how it looks, which is more usable than a unit test.

---
+++ src/Terminoid.Driver/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Terminoid.Driver
+{
+   class Program
+   {
+      static void Main( string[] args )
+      {
+         var items = new[]
+         {
+            "first",
+            "second",
+            "third",
+            "fourth",
+            "fifth",
+            "sixth",
+            "seventh",
+            "eighth",
+            "ninth",
+            "tenth",
+            "eleventh",
+            "twelfth",
+            "thirteenth",
+            "fourteenth",
+            "fifteenth"
+         };
+
+         var menu = new Menu( items );
+         int index = menu.Show( 10, 10 );
+
+         if ( index == -1 )
+         {
+            Console.WriteLine( "Cancelled" );
+         }
+         else
+         {
+            Console.WriteLine( $"Got {items[index]}" );
+         }
+
+         Console.Read();
+      }
+   }
+}
