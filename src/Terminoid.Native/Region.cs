88be67196d4089ce9b500d7920b69f0559f577e0
Alex Novak <alexwnovak@gmail.com>
2019-05-22 20:19:48 -0400

    Moving Region to Native project
    This commit doesn't build, because it also begins to move the rendering responsibility out of Region. It's getting moved to another class that doesn't exist yet, so whatever.

---
+++ src/Terminoid.Native/Region.cs
@@ -0,0 +1,23 @@
+﻿namespace Terminoid.Native
+{
+   public class Region
+   {
+      private readonly char[] _charBuffer;
+      private readonly ushort[] _attrBuffer;
+
+      public int Width { get; }
+      public int Height { get; }
+
+      public Region( int width, int height )
+      {
+         Width = width;
+         Height = height;
+
+         _charBuffer = new char[Width * Height];
+         _attrBuffer = new ushort[Width * Height];
+      }
+
+      internal char[] GetCharBuffer() => _charBuffer;
+      internal ushort[] GetAttrBuffer() => _attrBuffer;
+   }
+}
