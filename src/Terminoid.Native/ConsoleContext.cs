178f3094dc350946a6cd1666fb50c1f682861ece
Alex Novak <alexwnovak@gmail.com>
2019-05-22 20:21:51 -0400

    ConsoleContext's public API now only deals in regions
    It used to be the thing to broker low-level details back to callers. Now it can render regions, or read the console data INTO regions. Internally it uses the same stuff that was public, but are now implementation details that should be hidden at this low level.

--- src/Terminoid.Native/ConsoleContext.cs
+++ src/Terminoid.Native/ConsoleContext.cs
@@ -6,7 +6,7 @@ namespace Terminoid.Native
    {
       private static IntPtr _handle = NativeMethods.GetStdHandle( NativeMethods.STD_OUTPUT_HANDLE );
 
-      public unsafe static void WriteChars( int x, int y, ReadOnlySpan<char> slice, ReadOnlySpan<ushort> attr )
+      private unsafe static void WriteChars( int x, int y, ReadOnlySpan<char> slice, ReadOnlySpan<ushort> attr )
       {
          fixed ( char* ptr = slice )
          {
@@ -29,7 +29,7 @@ namespace Terminoid.Native
          }
       }
 
-      public static char[] ReadChars( int x, int y, int length )
+      private static char[] ReadChars( int x, int y, int length )
       {
          var buffer = new char[length];
 
@@ -43,7 +43,7 @@ namespace Terminoid.Native
          return buffer;
       }
 
-      public static ushort[] ReadAttrs( int x, int y, int length )
+      private static ushort[] ReadAttrs( int x, int y, int length )
       {
          var attrs = new ushort[length];
 
@@ -55,6 +55,37 @@ namespace Terminoid.Native
             out _ );
 
          return attrs;
+      }
+
+      public static Region Read( int x, int y, int width, int height )
+      {
+         var region = new Region( width, height );
+         var regionContext = new RegionContext( region );
+
+         for ( int row = 0; row < height; row++ )
+         {
+            var charBuffer = ReadChars( x, y + row, width );
+            regionContext.SetCharLine( row, charBuffer );
+
+            var attrBuffer = ReadAttrs( x, y + row, width );
+            regionContext.SetAttrLine( row, attrBuffer );
+         }
+
+         return region;
+      }
+
+      public static void Render( Region region, int x, int y )
+      {
+         var charBuffer = region.GetCharBuffer().AsSpan();
+         var attrBuffer = region.GetAttrBuffer().AsSpan();
+
+         for ( int row = 0; row < region.Height; row++ )
+         {
+            var charRow = charBuffer.Slice( row * region.Width, region.Width );
+            var attrRow = attrBuffer.Slice( row * region.Width, region.Width );
+
+            WriteChars( x, y + row, charRow, attrRow );
+         }
       }
    }
 }
