b269463db1fc978b012fb57fb33d7bb3bd66ca1b
Alex Novak <alexwnovak@gmail.com>
2019-05-25 14:18:21 -0400

    +Color abstraction
    It'd make sense to use ConsoleColor, except by making our own we can add a "transparent" option to indicate that the region's cell attribute should NOT be altered at all. Without this, it'd be much harder to read the attribute for a character we don't want to modify, only to blanket write all attributes.

---
+++ src/Terminoid.Tests/ColorTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Terminoid.Tests
+{
+   public class ColorTests
+   {
+      private class ColorDataProvider : IEnumerable<object[]>
+      {
+         private static object[] Case( Color color, int value ) => new object[] { color, value };
+
+         public IEnumerator<object[]> GetEnumerator()
+         {
+            yield return Case( Color.Transparent, -1 );
+            yield return Case( Color.Black, 0 );
+            yield return Case( Color.DarkBlue, 1 );
+            yield return Case( Color.DarkGreen, 2 );
+            yield return Case( Color.DarkCyan, 3 );
+            yield return Case( Color.DarkRed, 4 );
+            yield return Case( Color.DarkMagenta, 5 );
+            yield return Case( Color.DarkYellow, 6 );
+            yield return Case( Color.Gray, 7 );
+            yield return Case( Color.DarkGray, 8 );
+            yield return Case( Color.Blue, 9 );
+            yield return Case( Color.Green, 10 );
+            yield return Case( Color.Cyan, 11 );
+            yield return Case( Color.Red, 12 );
+            yield return Case( Color.Magenta, 13 );
+            yield return Case( Color.Yellow, 14 );
+            yield return Case( Color.White, 15 );
+         }
+
+         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+      }
+
+      [Theory]
+      [ClassData( typeof( ColorDataProvider ) )]
+      public void Value_EveryColor_HasCorrectValue( Color color, int value )
+      {
+         color.Value.Should().Be( value );
+      }
+   }
+}
---
+++ src/Terminoid/Color.cs
@@ -0,0 +1,33 @@
+﻿namespace Terminoid
+{
+   public struct Color
+   {
+      public static readonly Color Transparent = new Color( -1 );
+      public static readonly Color Black = new Color( 0 );
+      public static readonly Color DarkBlue = new Color( 1 );
+      public static readonly Color DarkGreen = new Color( 2 );
+      public static readonly Color DarkCyan = new Color( 3 );
+      public static readonly Color DarkRed = new Color( 4 );
+      public static readonly Color DarkMagenta = new Color( 5 );
+      public static readonly Color DarkYellow = new Color( 6 );
+      public static readonly Color Gray = new Color( 7 );
+      public static readonly Color DarkGray = new Color( 8 );
+      public static readonly Color Blue = new Color( 9 );
+      public static readonly Color Green = new Color( 10 );
+      public static readonly Color Cyan = new Color( 11 );
+      public static readonly Color Red = new Color( 12 );
+      public static readonly Color Magenta = new Color( 13 );
+      public static readonly Color Yellow = new Color( 14 );
+      public static readonly Color White = new Color( 15 );
+
+      internal int Value { get; }
+
+      private Color( int value ) => Value = value;
+
+      public override bool Equals( object obj )
+      {
+
+         return base.Equals( obj );
+      }
+   }
+}
