554df84a52d41935e993185eeb1473fb5792a7f0
Alex Novak <alexwnovak@gmail.com>
2019-05-25 14:37:07 -0400

    +ColorCell struct
    This encapsulates the data for one "cell" -- a character, a foreground color, and a background color. This way we can store an array of these items, rather than storing discrete arrays of differing data (chars vs. attributes). This helps with locality of reference, but also lets us deal in complete objects in one spot.

---
+++ src/Terminoid/ColorCell.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Terminoid
+{
+   public struct ColorCell
+   {
+      public char Char { get; }
+      public Color Foreground { get; }
+      public Color Background { get; }
+
+      public ColorCell( char c )
+         : this( c, Color.Gray, Color.Black )
+      {
+      }
+
+      public ColorCell( char c, Color foreground )
+         : this( c, foreground, Color.Black )
+      {
+      }
+
+      public ColorCell( char c, Color foreground, Color background )
+      {
+         Char = c;
+         Foreground = foreground;
+         Background = background;
+      }
+   }
+}
