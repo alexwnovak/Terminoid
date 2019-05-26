000d8ccde26133d809dcae3e2afdf8c209b30740
Alex Novak <alexwnovak@gmail.com>
2019-05-25 22:52:08 -0400

    +Viewport class
    This exposes properties to explain how to show something relative to the screen, even if you're at the bottom of the buffer. A (0,0) coordinate for top-left doesn't work in every circumstance, so this should be used instead.

---
+++ src/Terminoid/Viewport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Terminoid
+{
+   public static class Viewport
+   {
+      public static int Left => Console.WindowLeft;
+      public static int Top => Console.WindowTop;
+      public static int Right => Left + Console.WindowWidth;
+      public static int Bottom => Top + Console.WindowHeight;
+   }
+}
