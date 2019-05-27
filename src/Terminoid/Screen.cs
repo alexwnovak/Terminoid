080538f125b875c506e5126112cf8dd45d1287cc
Alex Novak <alexwnovak@gmail.com>
2019-05-27 10:32:00 -0400

    Child visual elements are now laid out relative to their parent

--- src/Terminoid/Screen.cs
+++ src/Terminoid/Screen.cs
@@ -6,16 +6,24 @@ namespace Terminoid
    {
       public static void Draw( VisualElement visualElement )
       {
+         DrawPositionally( visualElement, visualElement.Left, visualElement.Top );
+      }
+
+      private static void DrawPositionally( VisualElement visualElement, int left, int top )
+      {
          if ( !visualElement.IsInitialized )
          {
             visualElement.Initialize();
          }
 
-         ConsoleContext.Write( visualElement.Region, visualElement.Left, visualElement.Top );
+         ConsoleContext.Write( visualElement.Region, left, top );
 
          foreach ( var childElement in visualElement.Children )
          {
-            Draw( childElement );
+            int relativeChildLeft = visualElement.Left + childElement.Left;
+            int relativeChildTop = visualElement.Top + childElement.Top;
+
+            DrawPositionally( childElement, relativeChildLeft, relativeChildTop );
          }
       }
    }
