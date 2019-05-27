cae3f95c34d3560fb5e28db01e6754c0d85d1908
Alex Novak <alexwnovak@gmail.com>
2019-05-26 21:33:38 -0400

    +VisualElement base class
    This is the next layer up for abstraction. This hides the lower-level details of Regions and adds higher-level things like positioning, relationships to other items, etc. These are the objects that we'll chain together to make real items on the screen.

---
+++ src/Terminoid/VisualElement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Terminoid.Core;
+
+namespace Terminoid
+{
+   public abstract class VisualElement
+   {
+      internal bool IsInitialized { get; set; }
+      internal Region Region { get; private set; }
+
+      public int Width { get; private set; }
+      public int Height { get; private set; }
+
+      public int Left { get; set; }
+      public int Top { get; set; }
+
+      public List<VisualElement> Children { get; } = new List<VisualElement>();
+
+      protected VisualElement( int width, int height )
+      {
+         Width = width;
+         Height = height;
+      }
+
+      internal void Initialize()
+      {
+         Region = new Region( Width, Height );
+         var regionContext = new RegionContext( Region );
+
+         CreateRegion( regionContext );
+      }
+
+      protected abstract void CreateRegion( RegionContext rc );
+   }
+}
