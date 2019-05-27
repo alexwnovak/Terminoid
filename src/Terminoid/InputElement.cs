741548856e432271a5decfc3d7f4cb847f84a057
Alex Novak <alexwnovak@gmail.com>
2019-05-27 12:09:00 -0400

    +InputElement subclass of VisualElement
    This adds an input handler, allowing a visual element to have interactivity. This also implies you must have a visual aspect for input to work.

---
+++ src/Terminoid/InputElement.cs
@@ -0,0 +1,12 @@
+﻿namespace Terminoid
+{
+   public abstract class InputElement : VisualElement
+   {
+      protected InputElement( int width, int height )
+         : base( width, height )
+      {
+      }
+
+      protected internal abstract void InputHandler();
+   }
+}
