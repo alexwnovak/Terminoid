0912fecdc60ebf0b16e674d9374fdeab943487c7
Alex Novak <alexwnovak@gmail.com>
2019-05-26 21:36:17 -0400

    +Screen class
    This is the high-level interaction with the screen, which draws visual elements. It also separates behavior from data (where visual elements are only data, and this is the behavior for putting it on the screen).

---
+++ src/Terminoid/Screen.cs
@@ -0,0 +1,22 @@
+﻿using Terminoid.Core;
+
+namespace Terminoid
+{
+   public static class Screen
+   {
+      public static void Draw( VisualElement visualElement )
+      {
+         if ( !visualElement.IsInitialized )
+         {
+            visualElement.Initialize();
+         }
+
+         ConsoleContext.Write( visualElement.Region, visualElement.Left, visualElement.Top );
+
+         foreach ( var childElement in visualElement.Children )
+         {
+            Draw( childElement );
+         }
+      }
+   }
+}
