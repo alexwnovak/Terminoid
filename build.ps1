7cf5d347ddf445a20fbf286961e4d4e5a8e9e369
Alex Novak <alexwnovak@gmail.com>
2019-06-01 14:19:43 -0400

    Top-level build front end now calls the Module's build script

--- build.ps1
+++ build.ps1
@@ -1,18 +1,9 @@
-param (
-    [switch]
-    $ExitOnFailure
-)
+$oldLocation = $PWD
 
-if ( $ExitOnFailure ) {
+try {
     Set-Location $PSScriptRoot/src/Module
-    & build/Install-Dependencies.ps1
-    Invoke-psake
-    exit ( [int]( -not $psake.build_success ) )
-} else {
-    powershell -NoProfile -Args $PSScriptRoot -Command {
-        param ( $ScriptRoot )
-        Set-Location $ScriptRoot/src/Module
-        & build/Install-Dependencies.ps1
-        Invoke-psake
-    }
+    & ./build.ps1
+}
+finally {
+    Set-Location $oldLocation
 }
