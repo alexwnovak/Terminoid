48a1468d4df0f8f23f796f9024ab4e99a72b726b
Alex Novak <alexwnovak@gmail.com>
2019-06-01 14:21:34 -0400

    Adding back the exit on failure for appveyor

--- build.ps1
+++ build.ps1
@@ -1,8 +1,13 @@
+param (
+    [switch]
+    $ExitOnFailure
+)
+
 $oldLocation = $PWD
 
 try {
     Set-Location $PSScriptRoot/src/Module
-    & ./build.ps1
+    & ./build.ps1 -ExitOnFailure:$ExitOnFailure
 }
 finally {
     Set-Location $oldLocation
