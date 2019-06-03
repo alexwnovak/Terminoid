c78096575f5b10816fa82f20d4abe795a7849bcd
Alex Novak <alexwnovak@gmail.com>
2019-06-02 21:47:13 -0400

    Moving command completion to its own file

--- src/Module/tests/AutoCompletion/GitAutoCompletion.Tests.ps1
+++ src/Module/tests/AutoCompletion/GitAutoCompletion.Tests.ps1
@@ -1,25 +1,6 @@
 . $PSScriptRoot\..\Shared.ps1
 
 InModuleScope 'Terminoid' {
-    Describe 'Git command completion' {
-        Context 'No command is partially specified' {
-            It 'resolves command suggestions' {
-                $commandSuggestions = GitCommandCompleter -Command 'git '
-
-                $commandSuggestions[0].CompletionText | Should -Be 'add'
-                $commandSuggestions[1].CompletionText | Should -Be 'checkout'
-            }
-        }
-
-        Context 'A command is partially specified' {
-            It 'resolves partial matches' {
-                $commandSuggestions = GitCommandCompleter -Command 'git ad'
-
-                $commandSuggestions.CompletionText | Should -Be 'add'
-            }
-        }
-    }
-
     Describe 'Git Add completion' {
         Context 'Adding files with pending changes' {
             Mock GetModifiedFiles { 'one.txt', 'two.txt' }
