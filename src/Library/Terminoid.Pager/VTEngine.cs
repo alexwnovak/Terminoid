500c01fffbeca095d8fc2bbbbd369abdcb1ac9cc
Alex Novak <alexwnovak@gmail.com>
2019-06-08 13:53:20 -0400

    +VTEngine that receives Highlighter input and formats with VT sequences

---
+++ src/Library/Terminoid.Pager/VTEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Highlight.Engines;
+using Highlight.Patterns;
+
+namespace Terminoid.Pager
+{
+   internal class VTEngine : Engine
+   {
+      protected override string ProcessBlockPatternMatch( Definition definition, BlockPattern pattern, Match match )
+      {
+         throw new NotImplementedException();
+      }
+
+      protected override string ProcessMarkupPatternMatch( Definition definition, MarkupPattern pattern, Match match )
+      {
+         throw new NotImplementedException();
+      }
+
+      protected override string ProcessWordPatternMatch( Definition definition, WordPattern pattern, Match match )
+      {
+         return VTSequencer.Colorize( match.Value, pattern.Style.Colors );
+      }
+   }
+}
