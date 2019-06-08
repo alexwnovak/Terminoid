1b140c9de2e90a230637e85f88206cd936e8fac6
Alex Novak <alexwnovak@gmail.com>
2019-06-08 13:54:05 -0400

    Fleshing out rough implementation for the pager

--- src/Library/Terminoid.Pager/Program.cs
+++ src/Library/Terminoid.Pager/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace Terminoid.Pager
+﻿using System;
+using System.IO;
+using Highlight;
+
+namespace Terminoid.Pager
 {
    internal static class Program
    {
-      private static void Main( string[] args )
+      private static int Main( string[] args )
       {
+         if ( args.Length == 0 || !File.Exists( args[0] ) )
+         {
+            return 1;
+         }
+
+         var highlighter = new Highlighter( new VTEngine() );
+
+         using ( var streamReader = new StreamReader(args[0] ) )
+         {
+            while ( !streamReader.EndOfStream )
+            {
+               string line = streamReader.ReadLine();
+               string highlightedCode = highlighter.Highlight( "C#", line );
+               Console.WriteLine( highlightedCode );
+            }
+         }
+
+         return 0;
       }
    }
 }
