99712d85a1b947b072424cd5b347863fe850ece4
Alex Novak <alexwnovak@gmail.com>
2019-06-08 13:52:41 -0400

    +VTSequencer that ingests a string and outputs the VT-formatted version

---
+++ src/Library/Terminoid.Pager/VTSequencer.cs
@@ -0,0 +1,17 @@
+﻿using Highlight.Patterns;
+
+namespace Terminoid.Pager
+{
+   internal static class VTSequencer
+   {
+      private const string _prefix = "\x1b[38;2;{0};{1};{2}m";
+      private const string _suffix = "\x1b[0m";
+
+      public static string Colorize( string input, ColorPair colors )
+      {
+         string foreground = string.Format( _prefix, colors.ForeColor.R, colors.ForeColor.G, colors.ForeColor.B );
+
+         return $"{foreground}{input}{_suffix}";
+      }
+   }
+}
