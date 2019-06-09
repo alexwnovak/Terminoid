9fe98f4ee5ba15a54cf1759adfcd3118cbd359b9
Alex Novak <alexwnovak@gmail.com>
2019-06-09 15:50:41 -0400

    Moving the rest of VT.ps1 to New-VTSequence

--- src/Module/public/New-VTSequence.ps1
+++ src/Module/public/New-VTSequence.ps1
@@ -1,3 +1,64 @@
+$Esc = [char]0x1B
+$EscPrefix = "$Esc`[0;"
+$EscPostfix = "$Esc`[0m"
+
+$ConsoleColorToVTTable = @(
+    0, # Black
+    4, # Blue
+    2, # Green
+    6, # Cyan
+    1, # Red
+    5, # Magenta
+    3, # Yellow
+    7  # Gray
+)
+
+function GetAsConsoleColor {
+    param (
+        [ConsoleColor]
+        $Color,
+
+        [string]
+        $ColorType
+    )
+
+    $indexModifier = switch ( $ColorType ) {
+        'Foreground' { 30 }
+        'Background' { 40 }
+    }
+
+    $colorIndex = [int] $Color
+    $colorValue = if ( $colorIndex -ge 8 ) {
+        $colorIndex = $colorIndex - 8
+        $ConsoleColorToVTTable[$colorIndex] + $indexModifier + 60
+    } else {
+        $ConsoleColorToVTTable[$colorIndex] + $indexModifier
+    }
+
+    "$EscPrefix$($colorValue)m"
+}
+
+function GetAsRgbArray {
+    param (
+        [Array]
+        $Rgb,
+
+        [string]
+        $ColorType
+    )
+
+    $modifier = switch ( $ColorType ) {
+        'Foreground' { 38 }
+        'Background' { 48 }
+    }
+
+    if ( $Rgb.Length -ne 3 ) {
+        throw 'An RGB color array must have 3 elements between 0 and 255'
+    }
+
+    "$($EscPrefix)38;2;$($Rgb[0]);$($Rgb[1]);$($Rgb[2])m"
+}
+
 function New-VTSequence {
     param (
         $Text,
@@ -9,12 +70,10 @@ function New-VTSequence {
 
     $prefix = if ( $Color -is [ConsoleColor] ) {
         GetAsConsoleColor $Color Foreground
-    }
-    elseif ( [Enum]::GetNames( [ConsoleColor] ) -contains $Color ) {
+    } elseif ( [Enum]::GetNames( [ConsoleColor] ) -contains $Color ) {
         $consoleColor = $Color -as [ConsoleColor]
         GetAsConsoleColor $consoleColor Foreground
-    }
-    elseif ( $Color -is [Array] ) {
+    } elseif ( $Color -is [Array] ) {
         GetAsRgbArray $Color Foreground
     } #else {
     #     Write-Host "Can't tell $Color $($Color.GetType())"
