f479e91c17ff06b358c09abc76af88ac0ab7e1ac
Alex Novak <alexwnovak@gmail.com>
2019-06-14 20:13:14 -0400

    Reset-SpecialChar can now reset everything and be forced past confirm

--- src/Module/tests/Reset-SpecialChar.Tests.ps1
+++ src/Module/tests/Reset-SpecialChar.Tests.ps1
@@ -3,7 +3,17 @@
 Describe 'Reset-SpecialChar' {
     It 'resets the bar joiner to its default value' {
         Set-SpecialChar -Type BarJoiner -Char 'something else'
-        Reset-SpecialChar -Type BarJoiner
-        Get-SpecialChar -Type BarJoiner | Should -Be $([char]0xE0B0)
+        Reset-SpecialChar -Type BarJoiner -Force
+        Get-SpecialChar -Type BarJoiner | Should -Not -Be 'something else'
+    }
+
+    It 'can reset all characters at once' {
+        Set-SpecialChar -Type BarJoiner -Char 'something else'
+        Set-SpecialChar -Type SelectionIndicator -Char 'something else'
+
+        Reset-SpecialChar -Force
+
+        Get-SpecialChar -Type BarJoiner | Should -Not -Be 'something else'
+        Get-SpecialChar -Type SelectionIndicator | Should -Not -Be 'something else'
     }
 }
