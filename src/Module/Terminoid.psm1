6dd72661bc5e27cf07c9e88461eb014a16623616
Alex Novak <alexwnovak@gmail.com>
2019-06-30 18:02:00 -0400

    Selectively registering a handler (Debug only)

--- src/Module/Terminoid.psm1
+++ src/Module/Terminoid.psm1
@@ -40,6 +40,10 @@ function RegisterKeyHandlers {
     Set-PSReadLineKeyHandler -Chord Ctrl+N -ScriptBlock ${function:Add-NewItemCommand} -BriefDescription 'TerminoidInsertNewItem' -Description 'Inserts a New-Item command'
     Set-PSReadLineKeyHandler -Chord Ctrl+Shift+N -ScriptBlock ${function:Add-NewDirectoryCommand} -BriefDescription 'TerminoidInsertNewDirectory' -Description 'Inserts a New-Item command for directories'
     Set-PSReadLineKeyHandler -Chord Ctrl+Shift+UpArrow -ScriptBlock ${function:Set-LocationToParent} -BriefDescription 'TerminoidNavigateUp' -Description 'Navigates up one directory'
+
+    if ( IsDebugMode ) {
+        Set-PSReadlineKeyHandler -Chord Ctrl+Spacebar -ScriptBlock ${function:Show-AutoCompletion}
+    }
 }
 
 Register-ArgumentCompleter -Native -CommandName git -ScriptBlock {
