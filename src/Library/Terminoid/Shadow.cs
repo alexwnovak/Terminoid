33c768e4678cfd1236d08a28882a7c83953f2860
Alex Novak <alexwnovak@gmail.com>
2019-07-11 22:09:59 -0400

    Trying out a shadow

---
+++ src/Library/Terminoid/Shadow.cs
@@ -0,0 +1,17 @@
+﻿using Terminoid.Core;
+
+namespace Terminoid
+{
+   public class Shadow : VisualElement
+   {
+      public Shadow( int width, int height )
+         : base( width, height )
+      {
+      }
+
+      protected override void CreateRegion( RegionContext rc )
+      {
+         rc.Fill( Color.DarkGray, Color.Black );
+      }
+   }
+}
