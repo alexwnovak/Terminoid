534c8747dd0a6893c11a0df376c156058dc431ed
Alex Novak <alexwnovak@gmail.com>
2019-07-11 22:11:43 -0400

    Stubbing out a text box control

---
+++ src/Library/Terminoid/Controls/TextBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Terminoid.Core;
+
+namespace Terminoid.Controls
+{
+   public class TextBox : InputElement
+   {
+      private string _text;
+
+      public TextBox( int width, int height )
+         : base( width, height )
+      {
+      }
+
+      protected override void CreateRegion( RegionContext rc )
+      {
+         rc.Fill( Color.White, Color.DarkRed );
+      }
+
+      protected override void OnPaint( RegionContext rc )
+      {
+         rc.SetLine( 0, _text );
+      }
+
+      protected override void InputHandler( InputContext inputContext )
+      {
+         if ( inputContext.KeyInfo.Key == ConsoleKey.Enter )
+         {
+            inputContext.Complete();
+            return;
+         }
+
+         _text += inputContext.KeyInfo.KeyChar;
+         Invalidate();
+      }
+   }
+}
