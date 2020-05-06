62160c4169229fe77a02dadf7295a8145d04fe3d
Alex Novak <alexwnovak@gmail.com>
2020-05-05 22:57:18 -0400

    Now simulating a cursor

--- LineInput/Program.cs
+++ LineInput/Program.cs
@@ -17,14 +17,14 @@ namespace LineInput
             while (true)
             {
 
-                theta += 0.1;
+                theta += 0.05;
                 opacity = 0.5 + 0.5 * Math.Sin(theta);
 
                 int r = (int)(255.0 * opacity);
                 int g = (int)(192.0 * opacity);
                 int b = 0;
 
-                Console.Write($"\x0D\x1B[48;2;{r};{g};{b}m           \x1B[0m");
+                Console.Write($"\x0D\x1B[48;2;{r};{g};{b}m \x1B[0m");
                 Thread.Sleep( 16 );
 
                 // var keyInfo = Console.ReadKey(true);
