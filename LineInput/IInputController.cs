f2d0f2fef4b731b07d7e422a95b8b9e6429405a1
Alex Novak <alexwnovak@gmail.com>
2020-05-06 13:15:00 -0400

    +Same event to the interface

--- LineInput/IInputController.cs
+++ LineInput/IInputController.cs
@@ -1,7 +1,10 @@
+using System;
+
 namespace LineInput
 {
     public interface IInputController
     {
+        event EventHandler InputChanged;
         string GetBuffer();
         void PressKey(char key);
     }
