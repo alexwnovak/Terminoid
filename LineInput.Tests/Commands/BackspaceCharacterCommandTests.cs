65be17274b70386f6e20796ae0876f8e7dd1ab79
Alex Novak <alexwnovak@gmail.com>
2020-05-10 10:10:00 -0400

    Laying down some basic ground state tests for backspacing

---
+++ LineInput.Tests/Commands/BackspaceCharacterCommandTests.cs
@@ -0,0 +1,58 @@
+using Xunit;
+using FluentAssertions;
+using LineInput;
+using LineInput.Commands;
+
+namespace LineInput.Tests.Commands
+{
+    public class BackspaceCharacterCommandTests
+    {
+        [Fact]
+        public void TheTextIsBlank_BackspacingACharacter_TextDoesNotChange()
+        {
+            var inputState = new InputState();
+            var inputStateManager = new InputStateManager(inputState);
+
+            inputStateManager.ExecuteCommand(new BackspaceCharacterCommand());
+
+            inputState.Text.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void TheTextIsBlank_BackspacingACharacter_CursorIndexDoesNotChange()
+        {
+            var inputState = new InputState();
+            var inputStateManager = new InputStateManager(inputState);
+
+            inputStateManager.ExecuteCommand(new BackspaceCharacterCommand());
+
+            inputState.CursorIndex.Should().Be(0);
+        }
+
+        [Fact]
+        public void ThereIsText_BackspacingAtTheStartOfTheLine_TheTextDoesNotChange()
+        {
+            var inputState = new InputState();
+            var inputStateManager = new InputStateManager(inputState);
+
+            inputStateManager.ExecuteCommand(new InsertCharacterCommand(), 'a');
+            inputStateManager.ExecuteCommand(new MoveCursorCommand(), -1);
+            inputStateManager.ExecuteCommand(new BackspaceCharacterCommand());
+
+            inputState.Text.Should().Be("a");
+        }
+
+        [Fact]
+        public void ThereIsText_BackspacingAtTheStartOfTheLine_TheCursorPositionDoesNotChange()
+        {
+            var inputState = new InputState();
+            var inputStateManager = new InputStateManager(inputState);
+
+            inputStateManager.ExecuteCommand(new InsertCharacterCommand(), 'a');
+            inputStateManager.ExecuteCommand(new MoveCursorCommand(), -1);
+            inputStateManager.ExecuteCommand(new BackspaceCharacterCommand());
+
+            inputState.CursorIndex.Should().Be(0);
+        }
+    }
+}
---
+++ LineInput/Commands/BackspaceCharacterCommand.cs
@@ -0,0 +1,9 @@
+namespace LineInput.Commands
+{
+    public class BackspaceCharacterCommand : IInputStateCommand
+    {
+        public void Execute(IWritableInputState inputState)
+        {
+        }
+    }
+}
