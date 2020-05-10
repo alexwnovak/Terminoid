5625ac7a7889c151b7ab9e27faf2b5cb5481c33b
Alex Novak <alexwnovak@gmail.com>
2020-05-10 16:11:33 -0400

    Fleshing out Clock implementation and tests

--- LineInput.Tests/Animation/ClockTests.cs
+++ LineInput.Tests/Animation/ClockTests.cs
@@ -1,11 +1,50 @@
+using System;
 using Xunit;
 using FluentAssertions;
 using LineInput.Animation;
+using LineInput.Tests.Internal;
 
 namespace LineInput.Tests.Animation
 {
     public class ClockTests
     {
+        [Fact]
+        public void ByDefault_WithoutModification_TheElapsedTimeIsZero()
+        {
+            var clock = new Clock();
 
+            clock.ElapsedTime.Should().Be(TimeSpan.Zero);
+        }
+
+        [Fact]
+        public void WithNoTime_OneSecondIsAdded_TheElapsedTimeBecomesOneSecond()
+        {
+            var clock = new Clock();
+
+            clock.AddTime(TimeSpan.FromSeconds(1));
+
+            clock.ElapsedTime.TotalSeconds.Should().Be(1);
+        }
+
+        [Fact]
+        public void WithOneSecondOnTheClock_AnotherSecondIsAdded_TheElapsedTimeBecomesTwoSeconds()
+        {
+            var clock = new Clock();
+
+            clock.AddTime(TimeSpan.FromSeconds(1));
+            clock.AddTime(TimeSpan.FromSeconds(1));
+
+            clock.ElapsedTime.TotalSeconds.Should().Be(2);
+        }
+
+        [Fact]
+        public void WithNoTime_OneSecondIsAdded_TheClockIsNotifiedOfTheNewOneSecond()
+        {
+            var clock = new ClockDouble();
+
+            clock.AddTime(TimeSpan.FromSeconds(1));
+
+            clock.AddedTime.TotalSeconds.Should().Be(1);
+        }
     }
 }
--- LineInput/Animation/Clock.cs
+++ LineInput/Animation/Clock.cs
@@ -1,7 +1,17 @@
+using System;
+
 namespace LineInput.Animation
 {
     public class Clock
     {
-        
+        public TimeSpan ElapsedTime { get; private set; }
+
+        public void AddTime(TimeSpan timeSpan)
+        {
+            ElapsedTime += timeSpan;
+            OnTimeAdded(timeSpan);
+        }
+
+        protected virtual void OnTimeAdded(TimeSpan timeSpan) { }
     }
 }
