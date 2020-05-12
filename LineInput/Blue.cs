2905b2dd0eacd59fa716ab7afabb94ef3db587ab
Alex Novak <alexwnovak@gmail.com>
2020-05-11 23:17:27 -0400

    Same thing for blue

--- LineInput/Blue.cs
+++ LineInput/Blue.cs
@@ -1,8 +1,30 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
 namespace LineInput
 {
-    public class Blue
+    [ExcludeFromCodeCoverage]
+    public class Blue : IEquatable<Blue>
     {
         public byte Value { get; }
+
         internal Blue(byte value) => Value = value;
+
+        public override int GetHashCode() => Value;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Blue blue)
+            {
+                return Equals(blue);
+            }
+
+            return false;
+        }
+
+        public bool Equals(Blue other) => Value == other.Value;
+
+        public static bool operator ==(Blue lhs, Blue rhs) => lhs.Equals(rhs);
+        public static bool operator !=(Blue lhs, Blue rhs) => !lhs.Equals(rhs);
     }
 }
