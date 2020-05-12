cdc53c6bc3f4015c132e937404416c098e6066bd
Alex Novak <alexwnovak@gmail.com>
2020-05-11 23:13:03 -0400

    Giving Red the struct treatment

--- LineInput/Red.cs
+++ LineInput/Red.cs
@@ -1,8 +1,30 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
 namespace LineInput
 {
-    public struct Red
+    [ExcludeFromCodeCoverage]
+    public struct Red : IEquatable<Red>
     {
         public byte Value { get; }
+
         internal Red(byte value) => Value = value;
+
+        public override int GetHashCode() => Value;
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Red red)
+            {
+                return Equals(red);
+            }
+
+            return false;
+        }
+
+        public bool Equals(Red other) => Value == other.Value;
+
+        public static bool operator ==(Red lhs, Red rhs) => lhs.Equals(rhs);
+        public static bool operator !=(Red lhs, Red rhs) => !lhs.Equals(rhs);
     }
 }
