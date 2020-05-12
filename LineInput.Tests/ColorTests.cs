aca90d8b63ec5819a34434ca145b48308cb29d87
Alex Novak <alexwnovak@gmail.com>
2020-05-11 22:07:46 -0400

    Implementing equality checks

--- LineInput.Tests/ColorTests.cs
+++ LineInput.Tests/ColorTests.cs
@@ -15,5 +15,41 @@ namespace LineInput.Tests
             color.G.Should().Be(2);
             color.B.Should().Be(3);
         }
+
+        [Fact]
+        public void TwoDifferentColorsExistButHaveSameValues_ComparingForEquality_ColorsAreEqual()
+        {
+            var one = new Color(1, 2, 3);
+            var two = new Color(1, 2, 3);
+
+            one.Equals(two).Should().Be(true);
+        }
+
+        [Fact]
+        public void TwoDifferentColorsExistButHaveSameValues_ComparingForEqualityUsingOperator_ColorsAreEqual()
+        {
+            var one = new Color(1, 2, 3);
+            var two = new Color(1, 2, 3);
+
+            (one == two).Should().Be(true);
+        }
+
+        [Fact]
+        public void TwoDifferentColorsExistAndHaveDifferentValues_ComparingForEquality_ColorsAreNotEqual()
+        {
+            var one = new Color(1, 2, 3);
+            var two = new Color(7, 8, 9);
+
+            one.Equals(two).Should().Be(false);
+        }
+
+        [Fact]
+        public void TwoDifferentColorsExistButHaveDifferentValues_ComparingForEqualityUsingOperator_ColorsAreNotEqual()
+        {
+            var one = new Color(1, 2, 3);
+            var two = new Color(7, 8, 9);
+
+            (one != two).Should().Be(true);
+        }
     }
 }
--- LineInput/Color.cs
+++ LineInput/Color.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+
 namespace LineInput
 {
-    public struct Color
+    public struct Color : IEquatable<Color>
     {
         public byte R { get; }
         public byte G { get; }
@@ -12,5 +15,30 @@ namespace LineInput
             G = g;
             B = b;
         }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Color color)
+            {
+                return Equals(color);
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            // TODO: write your implementation of GetHashCode() here
+            throw new System.NotImplementedException();
+            return base.GetHashCode();
+        }
+
+        public bool Equals(Color other)
+        {
+            return R == other.R && G == other.G && B == other.B;
+        }
+
+        public static bool operator ==(Color lhs, Color rhs) => lhs.Equals(rhs);
+        public static bool operator !=(Color lhs, Color rhs) => !lhs.Equals(rhs);
     }
 }
