b0636728d771179a01774342c8cc065e41c0c6bf
Alex Novak <alexwnovak@gmail.com>
2020-05-13 23:06:17 -0400

    Can now insert/read a cell

--- LineInput.Tests/TextBufferTests.cs
+++ LineInput.Tests/TextBufferTests.cs
@@ -14,5 +14,15 @@ namespace LineInput.Tests
             textBuffer.Length.Should().Be(0);
         }
 
+        [Fact]
+        public void TheBufferIsInitiallyEmpty_AddingACell_TheNewCellCanBeReadByIndexerAndIsTheSameInstance()
+        {
+            var textBuffer = new TextBuffer();
+
+            var cell = new Cell('A');
+            textBuffer.Append(cell);
+
+            textBuffer[0].Should().Be(cell);
+        }
     }
 }
--- LineInput/TextBuffer.cs
+++ LineInput/TextBuffer.cs
@@ -7,5 +7,9 @@ namespace LineInput
         private readonly List<Cell> _cells = new List<Cell>();
 
         public int Length => _cells.Count;
+
+        public Cell this[int index] => _cells[index];
+
+        public void Append(Cell cell) => _cells.Add(cell);
     }
 }
