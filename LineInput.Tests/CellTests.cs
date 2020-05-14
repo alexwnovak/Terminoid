a2420be6462edcfb3e529002109164c24938f661
Alex Novak <alexwnovak@gmail.com>
2020-05-14 12:23:30 -0400

    +Test for passing a character through the constructor

--- LineInput.Tests/CellTests.cs
+++ LineInput.Tests/CellTests.cs
@@ -7,12 +7,21 @@ namespace LineInput.Tests
     public class CellTests
     {
         [Fact]
+        public void WhenCreatingACell_ItIsGivenACharacter_TheCharacterIsRetained()
+        {
+            var cell = new Cell('A');
+
+            cell.Char.Should().Be('A');
+        }
+
+        [Fact]
         public void ByDefault_CellsHaveDefaultGrayForeground()
         {
             var cell = new Cell('A');
 
             cell.Foreground.Should().BeEquivalentTo(new Color(127, 127, 127));
         }
+
 
     }
 }
