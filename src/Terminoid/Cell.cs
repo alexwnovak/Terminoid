e6a4a151a1ae8cbc9d978001e459cca03c495927
Alex Novak <alexwnovak@gmail.com>
2020-05-22 18:28:27 -0400

    Ditching unused default foreground property

--- src/Terminoid/Cell.cs
+++ src/Terminoid/Cell.cs
@@ -2,8 +2,6 @@ namespace LineInput
 {
     public class Cell
     {
-        public static readonly Color DefaultForeground = Color.FromRgb(127, 127, 127);
-
         public char Char { get; set; }
         public Color Foreground { get; set; } = Color.Empty();
         public Color Background { get; set; } = Color.Empty();
