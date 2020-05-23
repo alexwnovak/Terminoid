2ebfa60a689245a91db8ac0616b4b595a9125732
Alex Novak <alexwnovak@gmail.com>
2020-05-23 16:52:34 -0400

    Doing two-pi multiplication in line to remove a field

--- src/Terminoid/Animation/CursorAnimation.cs
+++ src/Terminoid/Animation/CursorAnimation.cs
@@ -4,16 +4,14 @@ namespace LineInput.Animation
 {
     public class CursorAnimation : Animatable
     {
-        private const double _twoPi = Math.PI * 2;
-
         public CursorAnimation(TimeSpan duration) : base(duration, RepeatBehavior.Forever)
         {
         }
 
         protected override void OnUpdate(int cursorIndex, TextBuffer textBuffer)
         {
-            byte g = (byte)(64 * Math.Sin(Progress * _twoPi) + 192);
-            byte b = (byte)(64 * Math.Sin(Progress * _twoPi) + 192);
+            byte g = (byte)(64 * Math.Sin(Progress * Math.PI * 2) + 192);
+            byte b = (byte)(64 * Math.Sin(Progress * Math.PI * 2) + 192);
 
             textBuffer[cursorIndex].Background = Color.FromRgb(0, g, b);
         }
