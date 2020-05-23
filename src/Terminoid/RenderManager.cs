ff4e0a82022356e6552f3b41fe84d27a911cd781
Alex Novak <alexwnovak@gmail.com>
2020-05-23 01:46:04 -0400

    Reordering methods to make more sense

--- src/Terminoid/RenderManager.cs
+++ src/Terminoid/RenderManager.cs
@@ -9,40 +9,18 @@ namespace LineInput
     {
         public static RenderManager Instance { get; } = new RenderManager();
 
+        private readonly List<Animatable> _animationObjects = new List<Animatable>();
+
         private bool _isInitialized;
         private bool _hasFocus;
+
         private Thread _renderThread;
+        private InputState _inputState;
 
         private bool _isThreadRunning;
 
-        public void Initialize()
+        private RenderManager()
         {
-            Console.WriteLine("===== Initializing");
-
-            if (!_isInitialized)
-            {
-                _isInitialized = true;
-                _renderThread = new Thread(RenderThreadProc);
-                _renderThread.IsBackground = true;
-                _renderThread.Start();
-            }
-        }
-
-        public void EnableFocus(InputState inputState)
-        {
-            _inputState = inputState;
-
-            var cursorAnimation = new CursorAnimation(TimeSpan.FromSeconds(0.5));
-            StartAnimation(cursorAnimation);
-
-            _hasFocus = true;
-        }
-
-        public void DisableFocus() => _hasFocus = false;
-
-        public void ShutDown()
-        {
-            _isThreadRunning = false;
         }
 
         private void RenderThreadProc()
@@ -84,21 +62,36 @@ namespace LineInput
             WriteTextBuffer(textBuffer);
         }
 
-        private InputState _inputState;
-        private readonly Thread _thread;
-
-
-        private readonly List<Animatable> _animationObjects = new List<Animatable>();
-
-        private RenderManager()
+        public void Initialize()
         {
+            Console.WriteLine("===== Initializing");
+
+            if (!_isInitialized)
+            {
+                _isInitialized = true;
+                _renderThread = new Thread(RenderThreadProc);
+                _renderThread.IsBackground = true;
+                _renderThread.Start();
+            }
         }
 
-        public RenderManager(InputState inputState)
+        public void EnableFocus(InputState inputState)
         {
             _inputState = inputState;
 
-            _thread = new Thread(ThreadProc);
+            var cursorAnimation = new CursorAnimation(TimeSpan.FromSeconds(0.5));
+            StartAnimation(cursorAnimation);
+
+            _hasFocus = true;
+        }
+
+        public void DisableFocus()
+        {
+            _hasFocus = false;
+        }
+
+        public void ShutDown()
+        {
             _isThreadRunning = false;
         }
 
@@ -133,42 +126,6 @@ namespace LineInput
             string rasterizedOutput = rasterizer.Rasterize(textBuffer);
 
             Console.Write($"\x0D{rasterizedOutput}");
-        }
-
-        private void ThreadProc(object parameter)
-        {
-            _isThreadRunning = true;
-            var lastTime = DateTime.Now;
-
-            var cursorAnimation = new CursorAnimation(TimeSpan.FromSeconds(0.5));
-            // var cursorAnimation = new BlinkCursorAnimation(Color.FromRgb(192, 0, 0), Color.FromRgb(255, 0, 0), TimeSpan.FromSeconds(1));
-            StartAnimation(cursorAnimation);
-
-            while (_isThreadRunning)
-            {
-                var elapsedTime = DateTime.Now - lastTime;
-
-                int cursorIndex;
-                TextBuffer textBuffer;
-
-                lock (_inputState)
-                {
-                    cursorIndex = _inputState.CursorIndex;
-                    textBuffer = _inputState.TextBuffer.Clone();
-                }
-
-                UpdateAnimation(elapsedTime, cursorIndex, textBuffer);
-
-                // Format and print
-
-                var rasterizer = new VTRasterizer();
-                string rasterizedOutput = rasterizer.Rasterize(textBuffer);
-
-                Console.Write($"\x0D{rasterizedOutput}");
-
-                lastTime = DateTime.Now;
-                Thread.Sleep(30);
-            }
         }
     }
 }
