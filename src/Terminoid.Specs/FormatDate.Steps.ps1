ecdf4b5ef00042d654b1c3b1a4a226cfc14245b6
Alex Novak <alexwnovak@gmail.com>
2020-05-25 20:34:56 -0400

    Refactoring out the parameter in Gherkin

--- src/Terminoid.Specs/FormatDate.Steps.ps1
+++ src/Terminoid.Specs/FormatDate.Steps.ps1
@@ -6,6 +6,8 @@ When 'I format the date' {
     $script:FormattedDate = Format-Date -Date $script:Date
 }
 
-Then "it should read 'just now'" {
-    $script:FormattedDate | Should -Be 'just now'
+Then "it should read '(.*)'" {
+    param ($Actual)
+
+    $script:FormattedDate | Should -Be $Actual
 }
