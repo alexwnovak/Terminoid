555806ac0b92d7122e557ebb1e8d86d95c2a128d
Alex Novak <alexwnovak@gmail.com>
2020-05-25 20:34:45 -0400

    Forgot to commit this one and I can't be bothered to fixup

--- src/Terminoid/Cmdlets/FormatDateCmdlet.cs
+++ src/Terminoid/Cmdlets/FormatDateCmdlet.cs
@@ -1,13 +1,20 @@
+using System;
 using System.Management.Automation;
 
 namespace Terminoid.Cmdlets
 {
     [Cmdlet(VerbsCommon.Format, "Date")]
+    [OutputType(typeof(string))]
     public class FormatDateCmdlet : PSCmdlet
     {
+        [Parameter(
+            Mandatory = true
+        )]
+        public DateTime Date { get; set; }
+
         protected override void ProcessRecord()
         {
-            WriteObject("Done");
+            WriteObject("just now");
         }
     }
 }
