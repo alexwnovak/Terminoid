9161d61089b858e500e21fe562c1a98290a21291
Alex Novak <alexwnovak@gmail.com>
2020-05-26 13:14:30 -0400

    Fleshing out the main algorithm for converting sizes

--- src/Terminoid.Specs/FormatBytes.feature
+++ src/Terminoid.Specs/FormatBytes.feature
@@ -10,11 +10,15 @@ Scenario: Piping in the size
     When I pipe the size to the formatter
     Then the formatted size should read '123 B'
 
-Scenario Outline: Formatting a variety of sizes in bytes
+Scenario Outline: Formatting a variety of whole sizes in different byte units
     Given I have a size of <size> bytes
     When I format the size
     Then the formatted size should read '<formattedSize>'
     Examples:
-    | size | formattedSize |
-    | 123  | 123 B         |
-
+    | size   | formattedSize |
+    | 123    | 123 B         |
+    | 123KB  | 123 KB        |
+    | 123MB  | 123 MB        |
+    | 123GB  | 123 GB        |
+    | 123TB  | 123 TB        |
+    | 123PB  | 123 PB        |
--- src/Terminoid/Cmdlets/FormatBytesCmdlet.cs
+++ src/Terminoid/Cmdlets/FormatBytesCmdlet.cs
@@ -5,6 +5,20 @@ namespace Terminoid.Cmdlets
     [Cmdlet(VerbsCommon.Format, "Bytes")]
     public class FormatBytesCmdlet : PSCmdlet
     {
+        private static readonly long[] _sizeTable = new[]
+        {
+            1L << 50,
+            1L << 40,
+            1L << 30,
+            1L << 20,
+            1L << 10,
+        };
+
+        private static readonly string[] _labelTable = new[]
+        {
+            "PB", "TB", "GB", "MB", "KB"
+        };
+
         [Parameter(
             Mandatory = true,
             Position = 0,
@@ -14,6 +28,16 @@ namespace Terminoid.Cmdlets
 
         protected override void ProcessRecord()
         {
+            for (int index = 0; index < _sizeTable.Length; index++)
+            {
+                if (Size > _sizeTable[index])
+                {
+                    double unitSize = Size / _sizeTable[index];
+                    WriteObject($"{unitSize} {_labelTable[index]}");
+                    return;
+                }
+            }
+
             WriteObject($"{Size} B");
         }
     }
