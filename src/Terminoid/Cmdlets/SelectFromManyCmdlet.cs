ed236cfc33eae464f5ac2baf65169073e906c878
Alex Novak <alexwnovak@gmail.com>
2020-06-02 17:12:28 -0400

    Now prints the basic list of choices, indicating the first by default

--- src/Terminoid/Cmdlets/SelectFromManyCmdlet.cs
+++ src/Terminoid/Cmdlets/SelectFromManyCmdlet.cs
@@ -6,9 +6,31 @@ namespace Terminoid.Cmdlets
     [Cmdlet(VerbsCommon.Select, "FromMany")]
     public class SelectFromManyCmdlet : PSCmdlet
     {
+        private int _selectedIndex = 0;
+
+        [Parameter(
+            Mandatory = true
+        )]
+        public object[] Choices { get; set; }
+
         protected override void ProcessRecord()
         {
-            Console.WriteLine("Select");
+            RenderChoices();
+        }
+
+        private void RenderChoices()
+        {
+            for (int index = 0; index < Choices.Length; index++)
+            {
+                if (index == _selectedIndex)
+                {
+                    Console.WriteLine($"* {Choices[index]}");
+                }
+                else
+                {
+                    Console.WriteLine($"  {Choices[index]}");
+                }
+            }
         }
     }
 }
