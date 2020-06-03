2c811430cce36837e389c127d3e175f56ea5e6d0
Alex Novak <alexwnovak@gmail.com>
2020-06-02 22:46:48 -0400

    Filling in a nicer looking selection than an asterisk indicator
    Would be cool to tie this to a "theme" later, where it drives what colors and shapes you get for this experience.

--- src/Terminoid/Cmdlets/SelectFromManyCmdlet.cs
+++ src/Terminoid/Cmdlets/SelectFromManyCmdlet.cs
@@ -52,11 +52,12 @@ namespace Terminoid.Cmdlets
             {
                 if (index == _selectedIndex)
                 {
-                    Console.WriteLine($"* {Choices[index]}");
+                    string pill = "\x1B[38;2;119;144;203m\uE0B6\x1B[38;2;255;255;255m\x1B[48;2;119;144;203m {0} \x1B[38;2;119;144;203m\x1B[49m\uE0B4\x1B[0m";
+                    Console.WriteLine(string.Format(pill, Choices[index]));
                 }
                 else
                 {
-                    Console.WriteLine($"  {Choices[index]}");
+                    Console.WriteLine($"  {Choices[index]}  ");
                 }
             }
         }
